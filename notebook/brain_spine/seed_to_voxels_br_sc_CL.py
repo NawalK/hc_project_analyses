3a7504378c2286f5c591306a3878dc124d8a436e
CarolineLndl <landelle.caroline@gmail.com>
2023-06-12 16:25:15 -0400

    add quadrant frostell masks

--- notebook/brain_spine/seed_to_voxels_br_sc_CL.py
+++ notebook/brain_spine/seed_to_voxels_br_sc_CL.py
@@ -3,7 +3,9 @@
 
 # ## <font color=#B2D732> <span style="background-color: #4424D6"> Imports
 
-# In[39]:
+# In[1]:
+#cd /cerebro/cerebro1/dataset/bmpd/derivatives/HealthyControls_project/hc_project_analyses/notebook/brain_spine/seed_to_voxels_br_sc_CL.py
+#nohup python /cerebro/cerebro1/dataset/bmpd/derivatives/HealthyControls_project/hc_project_analyses/notebook/brain_spine/seed_to_voxels_br_sc_CL.py /cerebro/cerebro1/dataset/bmpd/derivatives/HealthyControls_project/hc_project_analyses/notebook/brain_spine/ .pynohup.out &
 
 
 import sys,json
@@ -12,8 +14,7 @@ sys.path.append("/cerebro/cerebro1/dataset/bmpd/derivatives/HealthyControls_proj
 
 from seed_to_voxels import Seed2voxels
 
-
-# In[40]:
+# In[2]:
 
 
 ## <font color=#B2D732> <span style="background-color: #4424D6"> A/ Initialization
@@ -21,7 +22,7 @@ from seed_to_voxels import Seed2voxels
 
 #  ### <font color=#4424D6> I. Configuration & parameters </font >
 
-# In[143]:
+# In[20]:
 
 
 # Load config file ------------------------------------------------------------
@@ -29,21 +30,22 @@ with open('/cerebro/cerebro1/dataset/bmpd/derivatives/HealthyControls_project/hc
     config = json.load(config_file) # load config file should be open first and the path inside modified
     #config['list_subjects']=config["list_subjects_younger"]
 signal='raw'
+seed_indiv=False
 
 
 # ### <font color=#4424D6> II. Initialize class based on this </font>
 
-# In[144]:
+# In[21]:
 
 
-seed2voxels=Seed2voxels(config,signal) # initialize the function
+seed2voxels=Seed2voxels(config,signal,seed_indiv) # initialize the function
 
 
 # ## <font color=#B2D732> <span style="background-color: #4424D6"> B/ Data extraction </font></span>
 # ### <font color=#4424D6> I. Time series extraction - Target </font>
 # 
 
-# In[145]:
+# In[23]:
 
 
 target_timeseries,seeds_timeseries =seed2voxels.extract_data(redo=True,n_jobs=8) 
@@ -51,27 +53,53 @@ target_timeseries,seeds_timeseries =seed2voxels.extract_data(redo=True,n_jobs=8)
 
 # ## <font color=#B2D732> <span style="background-color: #4424D6"> C/ Correlation analysis
 
-# In[152]:
+# In[24]:
 
 
-
-
-# ## <font color=#B2D732> <span style="background-color: #4424D6"> D/ Distance correlation
-
-# In[ ]:
-
-
-output_img={};
-
+output_dir={};output_file={}; 
 for seed_name in config["seeds"]["seed_names"]:
-    output_img[seed_name]=[]
-    output_img[seed_name]=config['main_dir'] + config['seed2vox_dir'] + '/1_first_level/' + seed_name + '/' + config["targeted_voxels"]["target_name"]+ '_fc_maps/DistCorr_' + str(len(config['list_subjects'])) + 'subjects_seed_' + seed_name.split('_')[-1] + '_s'
-
+    output_dir[seed_name]=config['main_dir'] + config['seed2vox_dir'] + '/1_first_level/' + seed_name + '/' + config["targeted_voxels"]["target_name"]+ '_fc_maps/Corr/'
+    if not os.path.exists(output_dir[seed_name]):
+        os.mkdir(output_dir[seed_name])
     
-    dist_corr=seed2voxels.distance_corr_maps(seeds_timeseries["zscored"][seed_name],
+    output_file[seed_name]=  output_dir[seed_name] +'/corr_' + str(len(config['list_subjects'])) + 'subjects_seed_' + seed_name + '_s'
+    
+    # run correlation analysis
+    corr=seed2voxels.correlation_maps(seeds_timeseries["zmean"][seed_name],
+                                      target_timeseries["zscored"],
+                                      output_img=output_file[seed_name],
+                                      Fisher=True,
+                                      partial=False,
+                                      save_maps=True,
+                                      smoothing_output=None,
+                                      redo=True,
+                                      n_jobs=8)
+
+    #calculate the mean across participant
+    string="fslmaths " +output_file[seed_name] + " -Tmean " + output_file[seed_name].split(".")[0] + "_mean.nii.gz"
+    os.system(string)
+   
+
+
+
+
+output_dir={};output_file={};
+for seed_name in config["seeds"]["seed_names"]:
+    output_dir[seed_name]=config['main_dir'] + config['seed2vox_dir'] + '/1_first_level/' + seed_name + '/' + config["targeted_voxels"]["target_name"]+ '_fc_maps/MI/'
+   
+    if not os.path.exists(output_dir[seed_name]):
+            os.mkdir(output_dir[seed_name])
+    
+    output_file[seed_name]=  output_dir[seed_name] +'/mi_' + str(len(config['list_subjects'])) + 'subjects_seed_' + seed_name + '_ss'
+  
+    
+    mi=seed2voxels.mutual_info_maps(seeds_timeseries["zmean"][seed_name],
                                     target_timeseries["zscored"],
-                                    output_img=output_img[seed_name],
+                                    output_img=output_file[seed_name],
                                     save_maps=True,
                                     smoothing_output=[6,6,6],redo=True, n_jobs=8)
-
+    
+    #calculate the mean across participant
+    string="fslmaths " +output_file[seed_name] + " -Tmean " + output_file[seed_name].split(".")[0] + "_mean.nii.gz"
+    os.system(string)
 
